49df4557d3360a7d7ca9f50674dbc63b26ea1cbc
Amuthaaa <amuthapriya.guru28@gmail.com>
2017-06-22 15:12:39 +0530

    Create Roman.java

---
+++ Roman.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+public class Roman {
+	public static void main(String []args)
+	{
+		int i,l,num=0;
+		 String str;
+		 Scanner s=new Scanner(System.in);
+		 System.out.println("Enter the Roman");
+		 str=s.next();
+		 l=str.length();
+		 System.out.println(l);
+		  str=str.toUpperCase();
+		 System.out.println(str);
+		 for(i=0;i<l;i++)
+		 {
+			 System.out.println("Enter the Roman1");
+			 if(str.charAt(i)=='M')
+			 {
+				 System.out.println("Enter the Roman");
+				 num=num+1000;
+			 }
+			 else if(str.charAt(i)=='D')
+			 {
+				 num=num+500;
+			 }
+			 else if(str.charAt(i)=='C')
+			 {
+				 num=num+100;
+			 }
+			 else if(str.charAt(i)=='L')
+			 {
+
+				 if(i-1==-1&&str.charAt(i+1)=='X')
+				 {
+					 num=num+50;
+				 }
+				 
+				 else
+				 {
+					 num=num+50;
+				 }
+				 
+			 }
+			 
+			 else if(str.charAt(i)=='V')
+			 {
+				 
+				 if(i-1==-1&&str.charAt(i+1)=='I')
+				 {
+					 num=num+5;
+				 }
+				 
+				 else
+				 {
+					 num=num+5;
+				 }
+			 }
+			 else if(str.charAt(i)=='I')
+			 {
+				 
+				 num=num+1;
+			 }
+			 else if(str.charAt(i)=='X')
+			 {
+				 if(i-1!=-1&&str.charAt(i-1)=='I')
+				 {
+					 num=num+8;
+				 }
+				 
+				 else
+				 {
+					 num=num+10;
+				 }
+			 }
+			  
+		 }
+		 System.out.println(num); 
+		 
+	}
+
+	
+}
