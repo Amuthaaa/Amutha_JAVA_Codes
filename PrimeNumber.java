733df05dee709760bae7554c5adcd69416c6a9e4
Amuthaaa <amuthapriya.guru28@gmail.com>
2017-06-22 13:55:26 +0530

    Create PrimeNumber.java

---
+++ PrimeNumber.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class PrimeNumber {
+	public static void main(String []args)
+	{ 
+		int s1, s2, s3, flag = 0, i, j,count=0;
+        Scanner s = new Scanner(System.in);
+        System.out.println ("Enter the first limit :"); 
+        s1 = s.nextInt();
+        System.out.println ("Enter the seond limit :"); 
+        s2 = s.nextInt();
+        //System.out.println ("The prime numbers in between the entered limits are :");
+        for(i = s1; i <= s2; i++)
+        {
+            for( j = 2; j < i; j++)
+            {
+                if(i % j == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+                else
+                {
+                    flag = 1;
+                    
+                }
+            }
+            
+            if(flag!=0)
+            {
+                System.out.println(i);
+                count=count+1;
+             
+                
+            }  
+        }
+        
+        System.out.println(count);
+	}
+}
