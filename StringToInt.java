b8f0cb894cbf23485165f78c000399073e5e81db
Amuthaaa <amuthapriya.guru28@gmail.com>
2017-06-22 16:17:28 +0530

    Create StringToInt.java

---
+++ StringToInt.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class StringToInt {
+	public static void main(String []args)
+	{ 
+	    String str="123";
+	    Scanner s=new Scanner(System.in);
+	    int i=Integer.valueOf(str);
+	    System.out.println(i);
+	}
+}
